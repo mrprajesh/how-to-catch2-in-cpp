ce3387bfba8bf97a6783c8e5129694b8a3274e0f
Rajesh Pandian M <mrpraj@gmail.com>
2022-12-24 16:33:06 +0530

    make single eg fail initially

--- single/single-file-eg.cpp
+++ single/single-file-eg.cpp
@@ -1,11 +1,11 @@
-//g++ -std=c++11 -Wall single-file-eg.cpp -o single-file-eg.out -I ./lib/ && ./single-file-eg.out --success
+//g++ -std=c++11 -Wall single/single-file-eg.cpp -o single-file-eg.out -I ./lib/ && ./single-file-eg.out --success
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include <catch.hpp>
 
 unsigned int Factorial( unsigned int number ) {
-  // return number <= 1 ? number : Factorial( number - 1 ) * number;  // will fail
-  return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // will pass
+  return number <= 1 ? number : Factorial( number - 1 ) * number;  // will fail
+  // return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // will pass
 }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
